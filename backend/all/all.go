d2e49c10e75ff69cf924c579ead73ab80322ec9d
Christopher Fiola <36894544+ChrisFiola@users.noreply.github.com>
2024-07-12 17:08:02 -0400

    Update all.go

--- backend/all/all.go
+++ backend/all/all.go
@@ -44,7 +44,7 @@ import (
 	_ "github.com/rclone/rclone/backend/putio"
 	_ "github.com/rclone/rclone/backend/qingstor"
 	_ "github.com/rclone/rclone/backend/quatrix"
-	_ "github.com/ChrisFiola/rclone/backend/realdebrid"
+	_ "github.com/ChrisFiola/rclone/tree/master/backend/realdebrid"
 	_ "github.com/rclone/rclone/backend/s3"
 	_ "github.com/rclone/rclone/backend/seafile"
 	_ "github.com/rclone/rclone/backend/sftp"
